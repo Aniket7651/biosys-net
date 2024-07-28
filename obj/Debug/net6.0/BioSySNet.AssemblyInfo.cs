e149b58839ed824287ff61e3cb217fb897ee17ac
ANIKET YADAV <aniketyadav8687@gmail.com>
2024-07-28 18:38:34 +0530

    first s1

--- obj/Debug/net6.0/BioSySNet.AssemblyInfo.cs
+++ obj/Debug/net6.0/BioSySNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute(@"Bioinformatics is an interdisciplinary field that analyzes biological data using computer science. I have created a new package for bioinformatics and computational analysis built in C#. This package can analyze biological data such as DNA, RNA, and protein sequences, as well as perform tasks such as Biological Database Scraping. Additionally, 'GEO Analysis' offers a variety of analyses for RNA-seq preprocessing, parsing various biological file formats (FASTA, PDB, SOFT, FASTQ, etc.). BioSySNet also utilizes the Python interpreter to run certain plotting functions (i.e., it uses IronPython to execute Python functions, ""so first, you need to make sure that Python is installed on your machine"").")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42576bf4092caa4a551e26002aa41de47740260a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f113ffb32b0c23a4e751d5fb0330221d49d4cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioSySNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioSySNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
